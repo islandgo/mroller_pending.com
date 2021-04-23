36c8b41bf1d3d42d631610a5489cc5a8945f9552
islandgo <islandgoph@gmail.com>
2021-04-23 10:33:16 +0800

    update

--- wp-content/themes/isdtemplate.com/home.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-	<!-- your home html -->
-
-	<!-- your home html -->
-
-<?php get_footer(); ?>
---
+++ wp-content/themes/mroller_pending.com/home.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<!-- your home html -->
+
+<section class="hp-about-us">
+	<div class="comp-about-us">
+		<div class="section-name">
+			<h2>This is what we do</h2>
+			<h4>Our recent listed & sold properties</h4>
+		</div>
+
+		<a href="about" class="about-item">
+			<img alt="about" class="img-about"
+				src="<?php echo get_stylesheet_directory_uri() ?>/images/header-logo.png">
+			<h3>$1,000,000 & Up</h3>
+			<img alt="about" class="img-about"
+				src="<?php echo get_stylesheet_directory_uri() ?>/images/header-logo.png">
+			<h3>$750,000 - $1,000,000</h3>
+			<img alt="about" class="img-about"
+				src="<?php echo get_stylesheet_directory_uri() ?>/images/header-logo.png">
+			<h3>$500,000 - $750,000</h3>
+			<img alt="about" class="img-about"
+				src="<?php echo get_stylesheet_directory_uri() ?>/images/header-logo.png">
+			<h3>$500,000 & Below</h3>
+		</a>
+		<div class="about-item">
+			<div class="section-name">
+				<h2>This is how well we do it</h2>
+				<h4>What over 200 clients are saying about us</h4>
+			</div>
+			<a href="[blogurl]" class="search-btn-primary" aria-label="about-us">Read All About Us</a>
+		</div>
+	</div>
+</section>
+
+<!-- your home html -->
+
+<?php get_footer(); ?>
